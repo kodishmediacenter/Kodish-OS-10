f406dff5022930f1480122a46f2cfb995c44aeed
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-06-15 12:10:40 -0300

    Update script-archlinux10.sh

--- scripts-kodish-gamer/script-archlinux10.sh
+++ scripts-kodish-gamer/script-archlinux10.sh
@@ -208,6 +208,7 @@ pacman -S --noconfirm file-roller neofetch
 pacman -S --noconfirm nemo
 pacman -S --noconfirm wine wine-mono wine-gecko lib32-gnutls vulkan-icd-loader lib32-vulkan-icd-loader
 pacman -S --noconfirm fuse2
+pacman -S --noconfirm antimicrox
 
 
 
