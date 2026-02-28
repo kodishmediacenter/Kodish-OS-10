1f147dbfd95b5bfeb15597705b4bba921c86ff75
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-02-28 19:11:17 -0300

    Update flatpaks.sh

--- scripts-kodish-gamer/flatpaks.sh
+++ scripts-kodish-gamer/flatpaks.sh
@@ -4,9 +4,19 @@
 flatpak install flathub com.google.Chrome -y 
 flatpak install flathub com.heroicgameslauncher.hgl -y
 flatpak install flathub com.vysp3r.ProtonPlus -y 
+
 #flatpak install flathub net.retrodeck.retrodeck
 flatpak install flathub com.stremio.Stremio -y
 flatpak install flathub dev.aunetx.deezer -y 
 flatpak install flathub com.github.louis77.tuner -y
 flatpak install flathub org.libretro.RetroArch -y 
 flatpak install flathub it.mijorus.gearlever -y
+flatpak install flathub net.davidotek.pupgui2
+
+# Correção google chrome  
+flatpak override --user com.google.Chrome --filesystem=xdg-download
+flatpak override --user com.google.Chrome --filesystem=home
+sudo pacman -S xdg-desktop-portal xdg-desktop-portal-gtk
+flatpak override --user com.google.Chrome --filesystem=home
+systemctl --user status xdg-desktop-portal
+systemctl --user restart xdg-desktop-portal
