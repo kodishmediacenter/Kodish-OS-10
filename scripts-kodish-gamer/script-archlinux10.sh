661968a1b4fbd4ca3f8f84fa2f9fd94e304ddd52
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-07 12:08:39 -0300

    Update script-archlinux10.sh

--- scripts-kodish-gamer/script-archlinux10.sh
+++ scripts-kodish-gamer/script-archlinux10.sh
@@ -300,14 +300,11 @@ wget https://raw.githubusercontent.com/kodishmediacenter/Kodish-OS-10/refs/heads
 # Correções Permissões 
 chmod 777 /home/kodish/Desktop
 
-# Lock Kernel 
-echo "" >> /etc/pacman.conf
-echo "IgnorePkg = linux linux-headers linux-lts linux-lts-headers" >> /etc/pacman.conf
-
  pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
  pacman-key --lsign-key 3056513887B78AEB
  pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
  mv /etc/pacman.conf /etc/pacman.conf.bkp
+ cd /etc/
  wget https://raw.githubusercontent.com/kodishmediacenter/Kodish-OS-10/refs/heads/main/scripts-kodish-gamer/pacman.conf
  pacman -Syy
 
