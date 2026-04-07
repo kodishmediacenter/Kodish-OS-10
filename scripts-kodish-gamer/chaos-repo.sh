764c2d620e6b273f3e8a7da261bbb1b38c187bcc
kodishmediacenter <fabiolmglgeo@gmail.com>
2026-04-07 19:22:20 -0300

    Update chaos-repo.sh

--- scripts-kodish-gamer/chaos-repo.sh
+++ scripts-kodish-gamer/chaos-repo.sh
@@ -4,4 +4,5 @@ sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.ta
 sudo mv /etc/pacman.conf /etc/pacman.conf.bkp
 sudo cd /etc/
 sudo wget https://raw.githubusercontent.com/kodishmediacenter/Kodish-OS-10/refs/heads/main/scripts-kodish-gamer/pacman.conf
+sudo mv pacman.conf /etc
 sudo pacman -Syy
